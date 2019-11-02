ea675b35fcaaf55bf4a950a3c94288a2da22ef6c
kulia26 <kulinich261@gmail.com>
2019-11-02 10:16:14 +0200

    update module and npm files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'none',
+  entry: './src/app.js',
   output: {
-    filename: 'index.js',
+    filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/, 
+        loader: "babel-loader",
+        query: {
+          presets: [ 'es2015'],
+        }
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 9000
+  }
 };
