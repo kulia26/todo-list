4d9696d4f3133c4eabe201e87b006b0765a95a05
kulia26 <kulinich261@gmail.com>
2019-11-03 20:59:44 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'none',
   entry: './src/app.js',
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: 'style.css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
@@ -19,7 +28,18 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
       },
     ]
   },
